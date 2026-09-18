5f7a877543c7981da67bdce79604b4d741ea667c
Sastha Sakia Barus <sastabaruss@gmail.com>
2026-09-18 22:30:43 +0700

    Praktikum 1 - proyek Java pertama

---
+++ src/com/pbi/tk/HelloKlienServer.java
@@ -0,0 +1,17 @@
+
+package com.pbi.tk;
+
+
+public class HelloKlienServer {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("Nama      : Sastha Sakia Barus");
+        System.out.println("NIM       : 25110205");
+        System.out.println("Kelas     : Teknik Komputer-C Pagi");
+        System.out.println("==================================");
+        System.out.println("Aplikasi Klien siap dijalankan"); 
+    }
+    
+}
